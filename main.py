c9f9fd5e7756a41df6438fd03368f3a08334afb7
ESKARABATOS <bartek31234541@gmail.com>
2023-12-28 13:47:50 +0100

    wiki

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ from discord.ext import commands #importujemy obsługiwanie komend
 intents = discord.Intents.all() #Uprawniamy bota do wszystkich poleceń
 
 client = commands.Bot(commands_prefix="NAZWA PREFIXU KOMEND", intents=intents) #tworzenie klienta bota
+
+client.run("TOKEN") #URUCHAMIAMY BOTA TOKEN ZNAJDZIESZ NA DISCORD DEVELOPER PORTAL
