62121dd3fd4c1e003fa20ddbde2130be33b06e0b
ESKARABATOS <bartek31234541@gmail.com>
2023-12-28 13:40:21 +0100

    wiki

---
+++ main.py
@@ -0,0 +1,6 @@
+import discord #importujemy biblioteke discorda
+from discord.ext import commands #importujemy obsługiwanie komend
+
+intents = discord.Intents.all() #Uprawniamy bota do wszystkich poleceń
+
+client = commands.Bot(commands_prefix="NAZWA PREFIXU KOMEND", intents=intents) #tworzenie klienta bota
