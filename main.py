ac2f50d752b9c9d09b174151107e7e1b26d70958
ESKARABATOS <bartek31234541@gmail.com>
2023-12-28 13:51:57 +0100

    wiki

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ intents = discord.Intents.all() #Uprawniamy bota do wszystkich poleceń
 
 client = commands.Bot(commands_prefix="NAZWA PREFIXU KOMEND", intents=intents) #tworzenie klienta bota
 
+@client.event #tworzenie eventu
+async def on_ready(): #tworzenie funkcji eventu on_ready czyli po uruchomieniu bota 
+    print("Bot został uruchomiony")
+
 client.run("TOKEN") #URUCHAMIAMY BOTA TOKEN ZNAJDZIESZ NA DISCORD DEVELOPER PORTAL
