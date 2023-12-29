16f04a7eda056403d369ad09695acc65cb2a4d44
ESKARABATOS <bartek31234541@gmail.com>
2023-12-29 17:24:47 +0100

    wiki

--- commands/on_message.py
+++ commands/on_message.py
@@ -1,23 +1,23 @@
 #Tworzenie eventu
 #Główny plik
-@client.event
-async def on_message(): #ta funkcja uruchamia się tylko w tedy kiedy ktokolwiek wyśle wiadomość na kanale 
+#@client.event
+#async def on_message(): #ta funkcja uruchamia się tylko w tedy kiedy ktokolwiek wyśle wiadomość na kanale 
     #KONTYNUACJA KODU
 
 #W Wielu plikach w schemacie wiki
-@commands.Cog.listener()
-async def on_message(self):
+#@commands.Cog.listener()
+#async def on_message(self):
     #KONTYNUACJA KODU
 
 #praktyczne zastosowanie
     
-@client.event()
-async def on_message(message):
-    print(message.content)
+#@client.event()
+#async def on_message(message):
+#    print(message.content)
 
-@commands.Cog.listener()
-async def on_message(self, message):
-    print(message.content)
+#@commands.Cog.listener()
+#async def on_message(self, message):
+#    print(message.content)
 
 #tą funkcje można wykorzystać do 
 #liczenia wiadomości 
